ce149fe89fab2ddb02ed03525e91595e1806cf01
Fugui.Ma <957744822@qq.com>
2024-06-08 21:42:20 +0800

    Third commit

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -9,6 +9,7 @@ public class HelloworldApplication {
     public static void main(String[] args) {
 
         System.out.println("Hello git");
+        System.out.println("Hello git3");
         SpringApplication.run(HelloworldApplication.class, args);
     }
 
