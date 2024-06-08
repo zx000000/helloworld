3d0f5a40cb5b9945f97c6337c1c010c3b1255ea7
Fugui.Ma <957744822@qq.com>
2024-06-08 21:41:45 +0800

    Second commit

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello git");
         SpringApplication.run(HelloworldApplication.class, args);
     }
 
